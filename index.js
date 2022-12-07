a5dad696a198704e5b7ea4256a4682dc60150f39
Luiz Henz <robertgito@hotmail.com>
2022-12-07 20:15:03 -0300

    primeiro-up

--- index.js
+++ index.js
@@ -1,30 +1,60 @@
 const mostrar = document.getElementById("jogos");
+const mostraBanner = document.getElementById("banner");
+const btn = document.getElementById("botaoCarregar");
 
-function mostrarJogos(resposta){
+let primeiraP = 0;
+let segundaP = 0;
 
-    resposta.slice(0, 6).forEach(function(item){
+function mostrarJogos(resposta) {
+
+    mostraBanner.innerHTML = `<div>
+                                    <a href="${resposta[0].game_url}">
+                                    <img src="${resposta[0].thumbnail}">
+                                    </a>
+                                </div>`
+
+    resposta.slice(primeiraP, primeiraP += 6).forEach(function (item) {
 
         mostrar.innerHTML += `<div>
                             <a href="${item.game_url}">
                             <img src="${item.thumbnail}">
                             <p>${item.title}</p>
                             <p>${item.short_description}</p>
+                            <p>${item.genre}</p>
                             </a>
                         </div>`
     })
 
+    function carregar(){
     
+        resposta.slice(primeiraP, primeiraP += 10).forEach(function (item) {
+    
+            mostrar.innerHTML += `<div>
+                                <a href="${item.game_url}">
+                                <img src="${item.thumbnail}">
+                                <p>${item.title}</p>
+                                <p>${item.short_description}</p>
+                                <p>${item.genre}</p>
+                                </a>
+                                </div>`
+        })
+    }
+    
+    btn.addEventListener("click", carregar);
 }
 
+
+
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'ed04ba597fmsh91f8cb322f43b88p185c74jsn6aa29c859746',
-		'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'ed04ba597fmsh91f8cb322f43b88p185c74jsn6aa29c859746',
+        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+    }
 };
 
 fetch('https://free-to-play-games-database.p.rapidapi.com/api/games', options)
-	.then(resposta => resposta.json())
+    .then(resposta => resposta.json())
     .then(data => mostrarJogos(data))
-	.catch(err => console.error(err));
+    .catch(err => console.error(err));
+
