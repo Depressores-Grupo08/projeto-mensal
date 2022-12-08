3eee530144828bbfc42f268dca2dfc1378694ffc
ferreiraluc <l.adrianobf@gmail.com>
2022-12-08 20:43:19 -0300

    update-js

--- index.js
+++ index.js
@@ -1,38 +1,125 @@
-
 const mostrar = document.getElementById("jogos");
+const mostraBanner = document.getElementById("banner");
+const printFav = document.getElementById("");
+const btn = document.getElementById("botaoCarregar");
 
-function mostrarJogos(resposta){
+let primeiraP = 0;
+let segundaP = 0;
 
-    resposta.slice(0, 6).forEach(function(item){
+
+function mostrarJogos(resposta) {
+
+    mostraBanner.innerHTML += `<div class="slide s1">
+                                    <a href="${resposta[0].game_url}">
+                                    <img src="${resposta[0].thumbnail}">
+                                    </a>
+                                </div>`
+
+
+
+    resposta.slice(primeiraP, primeiraP += 6).forEach(function (item) {
 
         mostrar.innerHTML += `<div id="div_jogos">
-                            <a id="link_jogo"href="${item.game_url}">
+                            <a id="link_jogo" href="${item.game_url}">
                             <img id="imagem_jogo" src="${item.thumbnail}">
                             <p>${item.title}</p>
                             <p>${item.short_description}</p>
+                            <p>${item.genre}</p>
                             </a>
-                            <div id="div_comprar_favoritos">
-                            
-                                <button id="botao_comprar">JOGAR</button>
+                                <div id="div_comprar_favoritos">
+                                    <button id="botao_comprar">JOGAR</button>
 
-                                <input type="checkbox" id="input_favoritos">
-                                </input>
-                            </div>
-                        </div>`
+                                    <button id="input_favoritos" type="button"  value="${item.id}" onclick="salvarFavoritos(this)">Favoritar</button>
+                                </div>
+                            </div>`
     })
+    
 
+    function carregar(){
+    
+        resposta.slice(primeiraP, primeiraP += 10).forEach(function (item) {
+    
+            mostrar.innerHTML += `<div>
+                                <a href="${item.game_url}">
+                                <img src="${item.thumbnail}">
+                                <p>${item.title}</p>
+                                <p>${item.short_description}</p>
+                                <p>${item.genre}</p>
+                                </a>
+                                <button type="button"  value="${item.id}" onclick="salvarFavoritos(this)">Favoritar</button>
+                                </div>`
+        })
+    }
+
+    btn.addEventListener("click", carregar);
+
+}
+
+function salvarFavoritos(identidade) {
+
+    let recArray = localStorage.getItem('identidades');
+
+    if(recArray == undefined){
+        localStorage.setItem('identidades', JSON.stringify([]));
+    }
+
+    let novoArray = JSON.parse(recArray);
+
+    let verifica = novoArray.indexOf(identidade.value);
+    console.log(achei)
+
+    if(verifica != -1){
+        novoArray.splice(verifica, 1);
+        localStorage.setItem('identidades', JSON.stringify(novoArray));
+        return;
+    }
+    novoArray.push(identidade.value);
+    localStorage.setItem('identidades', JSON.stringify(novoArray));
+    console.log(novoArray);
+}
+
+function printarFavoritos(){
     
 }
 
+
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'ed04ba597fmsh91f8cb322f43b88p185c74jsn6aa29c859746',
-		'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'ed04ba597fmsh91f8cb322f43b88p185c74jsn6aa29c859746',
+        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+    }
 };
 
 fetch('https://free-to-play-games-database.p.rapidapi.com/api/games', options)
-	.then(resposta => resposta.json())
+    .then(resposta => resposta.json())
     .then(data => mostrarJogos(data))
-	.catch(err => console.error(err));
+    .catch(err => console.error(err));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
