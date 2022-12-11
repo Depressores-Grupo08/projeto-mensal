20b651278191f743fe0f34a785e4c9945e203c25
Luiz Henz <robertgito@hotmail.com>
2022-12-10 23:08:16 -0300

    up-favoritos-filtro

--- index.js
+++ index.js
@@ -1,81 +1,112 @@
 const mostrar = document.getElementById("jogos");
 const mostraBanner = document.getElementById("banner");
-const printFav = document.getElementById("")
+const printFav = document.getElementById("favoritos")
 const btn = document.getElementById("botaoCarregar");
+
 
 let primeiraP = 0;
 let segundaP = 0;
+let TODOS_JOGOS;
 
-function mostrarJogos(resposta) {
+function mostrarJogos() {
+
 
     mostraBanner.innerHTML = `<div>
-                                    <a href="${resposta[0].game_url}">
-                                    <img src="${resposta[0].thumbnail}">
+                                    <a href="${TODOS_JOGOS[0].game_url}">
+                                    <img src="${TODOS_JOGOS[0].thumbnail}">
                                     </a>
                                 </div>`
 
-    resposta.slice(primeiraP, primeiraP += 6).forEach(function (item) {
+    let exibirPrimeiros = TODOS_JOGOS.slice(primeiraP, primeiraP += 6)
+    exibirPrimeiros.forEach(function (exibirRes) {
 
         mostrar.innerHTML += `<div>
-                            <a href="${item.game_url}">
-                            <img src="${item.thumbnail}">
-                            <p>${item.title}</p>
-                            <p>${item.short_description}</p>
-                            <p>${item.genre}</p>
+                            <a href="${exibirRes.game_url}">
+                            <img src="${exibirRes.thumbnail}">
+                            <p>${exibirRes.title}</p>
+                            <p>${exibirRes.short_description}</p>
+                            <p>${exibirRes.genre}</p>
                             </a>
-                            <button type="button"  value="${item.id}" onclick="salvarFavoritos(this)">Favoritar</button>
+                            <button type="button"  value="${exibirRes.id}" onclick="salvarFavoritos(this)">Favoritar</button>
                         </div>`
     })
-    
-
-    function carregar(){
-    
-        resposta.slice(primeiraP, primeiraP += 10).forEach(function (item) {
-    
-            mostrar.innerHTML += `<div>
-                                <a href="${item.game_url}">
-                                <img src="${item.thumbnail}">
-                                <p>${item.title}</p>
-                                <p>${item.short_description}</p>
-                                <p>${item.genre}</p>
-                                </a>
-                                <button type="button"  value="${item.id}" onclick="salvarFavoritos(this)">Favoritar</button>
-                                </div>`
-        })
-    }
-
-    btn.addEventListener("click", carregar);
-
 }
 
-function salvarFavoritos(identidade) {
+function carregar() {
+
+    let exibirItens = TODOS_JOGOS.slice(primeiraP, primeiraP += 10)
+    exibirItens.forEach(function (exibir) {
+        mostrar.innerHTML += `<div>
+                            <a href="${exibir.game_url}">
+                            <img src="${exibir.thumbnail}">
+                            <p>${exibir.title}</p>
+                            <p>${exibir.short_description}</p>
+                            <p>${exibir.genre}</p>
+                            </a>
+                            <button type="button"  value="${exibir.id}" onclick="salvarFavoritos(this)">Favoritar</button>
+                            </div>`
+    })
+}
+btn.addEventListener("click", carregar);
+
+
+
+function salvarFavoritos(TODOS_JOGOS) {
 
     let recArray = localStorage.getItem('identidades');
 
-    if(recArray == undefined){
+    if (recArray == undefined) {
         localStorage.setItem('identidades', JSON.stringify([]));
     }
 
     let novoArray = JSON.parse(recArray);
+    console.log(novoArray)
+    let verifica = novoArray.indexOf(TODOS_JOGOS.value);
 
-    let verifica = novoArray.indexOf(identidade.value);
-    console.log(achei)
-
-    if(verifica != -1){
+    if (verifica != -1) {
         novoArray.splice(verifica, 1);
         localStorage.setItem('identidades', JSON.stringify(novoArray));
+        console.log(novoArray)
         return;
     }
-    novoArray.push(identidade.value);
+    novoArray.push(TODOS_JOGOS.value);
     localStorage.setItem('identidades', JSON.stringify(novoArray));
     console.log(novoArray);
+
 }
 
-function printarFavoritos(){
-    
+function printarFavoritos() {
+    let ids = localStorage.getItem('identidades');
+    ids = JSON.parse(ids);
+    let aux;
+    let fav = [];
+    for (let i = 0; i < ids.length; i++) {
+        let indice = TODOS_JOGOS.findIndex(p => p.id == ids[i]);
+        aux = TODOS_JOGOS[indice];
+        fav.push(aux);
+    }   
+
+    fav.forEach(function (itemFav) {
+        printFav.innerHTML += `<div>
+                                <a href="${itemFav.game_url}">
+                                <img src="${itemFav.thumbnail}">
+                                <p>${itemFav.title}</p>
+                                <p>${itemFav.short_description}</p>
+                                <p>${itemFav.genre}</p>
+                                </a>
+                                <button type="button"  value="${itemFav.id}" onclick="salvarFavoritos(this)">Favoritar</button>
+                            </div>`
+    })
+} printFav.addEventListener('click', printarFavoritos)
+
+function filtrar(categoria) {
+
+    let mostrarFiltrados = TODOS_JOGOS.filter((valorAtual) => {
+        return valorAtual.genre.includes(categoria);
+        
+    })
+    console.log(mostrarFiltrados);
 }
-
-
 const options = {
     method: 'GET',
     headers: {
@@ -86,6 +117,9 @@ const options = {
 
 fetch('https://free-to-play-games-database.p.rapidapi.com/api/games', options)
     .then(resposta => resposta.json())
-    .then(data => mostrarJogos(data))
+    .then(data => {
+        TODOS_JOGOS = data
+        mostrarJogos()
+    })
     .catch(err => console.error(err));
 
