cc13bde16fff8c7f5cf9560491f058d405229ad4
Luiz Henz <robertgito@hotmail.com>
2022-12-11 18:02:47 -0300

    up

--- index.js
+++ index.js
@@ -122,39 +122,3 @@ fetch('https://free-to-play-games-database.p.rapidapi.com/api/games', options)
         mostrarJogos()
     })
     .catch(err => console.error(err));
-
-
-
-
-
-
-// const mostrar = document.getElementById("jogos");
-
-// function mostrarJogos(resposta){
-
-//     resposta.slice(0, 6).forEach(function(item){
-
-//         mostrar.innerHTML += `<div id="div_jogos">
-//                             <a id="link_jogo"href="${item.game_url}">
-//                             <img id="imagem_jogo" src="${item.thumbnail}">
-//                             <p>${item.title}</p>
-//                             <p>${item.short_description}</p>
-//                             </a>
-//                         </div>`
-//     })
-
-    
-// }
-
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': 'ed04ba597fmsh91f8cb322f43b88p185c74jsn6aa29c859746',
-// 		'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-// 	}
-// };
-
-// fetch('https://free-to-play-games-database.p.rapidapi.com/api/games', options)
-// 	.then(resposta => resposta.json())
-//     .then(data => mostrarJogos(data))
-// 	.catch(err => console.error(err));
